c1f71a84753144c9b2e1616c4afc7dded7a67c09
Jack <Jack@10.68.168.193>
2017-04-24 16:31:22 -0600

    Creacion de Seleccionar Deporte y Variables de Sesion

--- web/select-sport.php
+++ web/select-sport.php
@@ -7,11 +7,37 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Seleccionar Deporte</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet"  type="text/css" href="css/styles.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <div class="row text-center text-uppercase text-primary"><h1 for="Deporte">Seleccionar Deporte:</h1></div>
+            <div class="row col-md-4 col-md-offset-4 text-center">
+                <div class="form-group">
+                    <select id="deporte">
+                        <option value="0">Futbol</option>
+                        <option value="1">Baloncesto</option>
+                        <option value="2">Voleibol</option>
+                        <option value="3">Natación</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select id="sexo">
+                        <option value="m">Masculino</option>
+                        <option value="f">Femenino</option>
+                    </select>
+                </div>
+            </div>
+
+
+            <?php
+                session_start();
+                $_SESSION["username"]=$_REQUEST["user"];
+                $_SESSION["password"]=$_REQUEST["pass"];
+            ?>
+            <script src="js/jquery-3.2.0.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
